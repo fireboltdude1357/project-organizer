b17c50d09b30c90f436dde71a3344babe9fb10de
Tanner Sharon <sharon.t@wustl.edu>
2026-01-16 19:43:59 -0600

    Initial Commit

---
+++ ProjectOrganizer/ProjectOrganizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectOrganizerApp.swift
+//  ProjectOrganizer
+//
+//  Created by Tanner Sharon on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct ProjectOrganizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
